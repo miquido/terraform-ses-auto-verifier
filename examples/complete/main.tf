672f12bf685d906efd074e8a65ddbe4f45fb0463
Marek Mościchowski <marek.moscichowski@miquido.com>
2021-09-01 11:12:46 +0200

    fix: fmt

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -1,8 +1,8 @@
 module "terraform-ses-auto-verifier" {
-  source     = "../../"
-  bucket_arn = "arn::bucket"
-  bucket_id  = "ses-bucket"
-  namespace  = "super-project"
-  stage      = "production"
+  source                = "../../"
+  bucket_arn            = "arn::bucket"
+  bucket_id             = "ses-bucket"
+  namespace             = "super-project"
+  stage                 = "production"
   mail_s3_bucket_prefix = "emails/bounces/example.com"
 }
